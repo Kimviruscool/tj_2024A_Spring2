3258d9dddfe60f9ad0ddbf54003bb86c1c6a6cde
Kimviruscool <sinsa9122@naver.com>
2024-09-10 18:05:03 +0900

    2024 - 09 - 10

--- build/resources/main/static/js/chat/chat.js
+++ build/resources/main/static/js/chat/chat.js
@@ -1,3 +1,3 @@
 console.log('chat.js')
-//JS : 클라이언트 웹소켓 (사용자)
+//JS : 클라이언트 웹소켓 (사용자) # new WebSocket("ws://localhost:8080/ws매핑주소")
 let clientSocket = new WebSocket("ws://localhost:8080/chat/conn")
